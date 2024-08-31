814eb660c4ee6073daef7eab576d43ec15d25b73
robert <robertndive29@gmail.com>
2024-08-31 03:43:51 +0100

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,13 +12,14 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-3xl sm:text-7xl font-bold text-white" id="mob">
-            I'm a Software Engineer skilled in web development and devoploment
-            of mobile applications
+            A pro-error Detector DEVELOPER Skilled In Programming And Coding of
+            Basic and Advanced Ideations
           </h2>
           <p className="text-gray-500 py-4 max-w-md" id="descrip">
             Welcome to the world of An Engineering Developer who is currently a
-            final year student in the University of Buea,I am capable of giving
-            life to your technical idealogical solutions
+            final year student of FET,University of Buea and very ready for the
+            Job market, I am capable of giving life to your technical
+            idealogical solutions
           </p>
 
           <div>
@@ -26,7 +27,7 @@ const Home = () => {
               to="portfolio"
               smooth
               duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-green-500 to-yellow-500 cursor-pointer"
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-blue-500 to-pink-500 cursor-pointer"
             >
               Portfolio
               <span className="group-hover:rotate-90 duration-600">
@@ -40,7 +41,7 @@ const Home = () => {
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-3xl mx-auto w-2/3 md:w-full"
+            className="rounded-3xl mx-auto w-2/4 md:w-full"
           />
         </div>
       </div>
